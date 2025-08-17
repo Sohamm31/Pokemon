71f68ccf2587029b126e739cef995681cc8ad69e
Sohamm31 <sohamm3107@gmail.com>
2025-08-17 20:30:15 +0530

    Frontend Decentralized

--- app/main.py
+++ app/main.py
@@ -1,28 +1,49 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from .routers import pokemon,users,teams
-from .database import engine
-from . import models
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
+from . import models
+from .database import engine
+from .routers import pokemon, users, teams
+
+# This line creates the database tables (if they don't exist)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-app.include_router(pokemon.router)
-app.include_router(users.router)
-app.include_router(teams.router)
-
-
+# Mount the static directory which will contain all your HTML files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# --- UPDATED ROUTING FOR SEPARATE HTML FILES ---
 
 @app.get("/", include_in_schema=False)
 async def read_index():
-    return FileResponse('app/static/index.html')
+    return FileResponse('app/static/landing.html')
 
-@app.get("/")
-async def read_route():
-    return {"Message":"Pokemon server started"}
+@app.get("/pokedex.html", include_in_schema=False)
+async def read_pokedex():
+    return FileResponse('app/static/pokedex.html')
+
+@app.get("/teams.html", include_in_schema=False)
+async def read_teams():
+    return FileResponse('app/static/teams.html')
+
+@app.get("/auth.html", include_in_schema=False)
+async def read_auth():
+    return FileResponse('app/static/auth.html')
+
+# Include all the API routers
+app.include_router(pokemon.router)
+app.include_router(users.router)
+app.include_router(teams.router)
 
